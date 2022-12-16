d3d6cf7ddc4062251ec22383602b2006b5b383f3
jnguyen127 <98630197+jnguyen127@users.noreply.github.com>
2022-12-16 00:47:42 -0500

    Add files via upload

---
+++ Staircase.lua
@@ -0,0 +1,16 @@
+function detect()
+    if turtle.detect() then
+        return 1
+    else 
+        return 0
+    end
+end 
+
+/* Main Program */
+for i = 0, 10, 1
+do  
+    if detect() then
+        turtle.dig()
+    end
+    turtle.forward()
+end
