49012d9fd76c6f43e9e269d06958779702ae540c
jnguyen127 <98630197+jnguyen127@users.noreply.github.com>
2022-12-16 00:14:56 -0500

    Add files via upload

--- Stair_Case_5x6.lua
+++ Stair_Case_5x6.lua
@@ -1,3 +1,16 @@
-turtle.digDown()
-turtle.turnLeft()
-turtle.turnLeft()
+function detect()
+    if turtle.detect() then
+        return 1
+    else 
+        return 0
+    end
+end 
+
+/* Main Program */
+for i = 0, 10, 1
+do  
+    if detect() then
+        turtle.dig()
+    end
+    turtle.forward()
+end
